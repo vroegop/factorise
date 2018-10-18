7fce580c73f28df79a54dd227999b9bd602b1e27
Randy (R.J.P.) Vroegop <randy.vroegop@ing.com>
2018-10-18 14:20:24 +0200

    feat(-polymer): Add polymer support

---
+++ factory-polymer.ts
@@ -0,0 +1,20 @@
+import { factorise } from "./factorise";
+
+export const Factory = (superClass: any) => class extends superClass {
+    public id: string = "";
+
+    public register(id: string) {
+        if (id) {
+            this.id = id;
+            factorise.registerFactory(this);
+        } else {
+            throw Error("Registering this factory without an event is not possible. "
+                + "A factory has to listen to an event. "
+                + "Please call the register method with an eventName: `register(eventName: String)`");
+        }
+    }
+
+    public do(oldState: any, payload: any) {
+        return { ...oldState, ...payload };
+    }
+};
---
+++ observer-polymer.ts
@@ -0,0 +1,31 @@
+import { factorise } from "./factorise";
+
+export const Observer = (superClass: any) => class extends superClass {
+    public newState: any = {};
+    public oldState: any = {};
+
+    public register() {
+        factorise.registerObserver(this);
+    }
+
+    /**
+     * This function is an example of the stateUpdate function.
+     * You can copy-paste this into your code if you extend the observer to implement updates.
+     * Not overriding sets newState and oldState as properties.
+     * That means that in Polymer for example, they are bound to the property.
+     * @param newState the new state object
+     * @param oldState the old state object
+     */
+    public stateUpdate(newState: any, oldState: any) {
+        this.newState = newState;
+        this.oldState = oldState;
+    }
+
+    public mapEventToFactory(element: HTMLElement, event: string, factory: string) {
+        element.addEventListener(event, (e) => factorise.execute(factory, (e as CustomEvent).detail));
+    }
+
+    public executeFactory(factory: string, payload: any) {
+        factorise.execute(factory, payload);
+    }
+};
